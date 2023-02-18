0f32bf5b63970348bcc3a7d454416bee0f6f282a
Eric Zhang <46907515+ericzhang11101@users.noreply.github.com>
2023-02-18 12:49:39 -0500

    auth setup

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import {googleAuth, googleProvider} from '../../firebase/services'
+
 
 export default function Login() {
+  
+  async function handleSignIn(e){
+      e.preventDefault()
+
+      const login = await googleAuth(googleProvider)
+      console.log(login)
+     
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      Login
+      <button onClick={handleSignIn}>
+        click me
+      </button>
+    </div>
   )
 }
---
+++ frontend/src/firebase/services.js
@@ -0,0 +1,25 @@
+import {auth} from './firebase-config'
+
+export const googleAuth = function(provider) {  
+    
+    console.log('google provider: ')
+    console.log(provider)
+
+    return auth().signInWithPopup(provider).then((res) => {
+        return res.user
+    }).catch((err) =>{
+        return err;
+    })
+
+}
+
+export const logout = function() {
+        auth().signOut().then(() =>{
+        console.log('signed out user')
+        return true;
+    }).catch(() => {
+        return false
+    })
+}
+
+export const googleProvider = new auth.GoogleAuthProvider()
