4e26d72148df3d03e26b871be32efe7bc153ef63
Eric Zhang <46907515+ericzhang11101@users.noreply.github.com>
2023-02-21 13:33:26 -0500

    my courses setup

--- frontend/src/components/pages/MyCourses.jsx
+++ frontend/src/components/pages/MyCourses.jsx
@@ -1,9 +1,25 @@
 import React, {useState} from 'react'
 import { Stack } from '@mui/system'
-import { Divider, Card, CardContent, Typography, Collapse, IconButton} from '@mui/material'
+import { Card, CardContent, Typography, Collapse, IconButton, Button} from '@mui/material'
 import {ExpandMore } from '@mui/icons-material'
 import { styled } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
 
+const currCourses = {
+  term: "3A",
+  courses: [
+    {
+      courseCode: 342,
+      programCode: "MSCI",
+      name: "Introduction to bofa my nuts"
+    },
+    {
+      courseCode: 334,
+      programCode: "MSCI",
+      name: "ur mom"
+    }
+  ]
+}
 
 const ExpandMoreComponent = styled((props) => {
   const { expand, ...other } = props;
@@ -18,8 +34,12 @@ const ExpandMoreComponent = styled((props) => {
 
 
 export default function MyCourses() {
-  const [showCurrentCourses, setShowCurrentCourses] = useState(false)
+  const [showCurrentCourses, setShowCurrentCourses] = useState(true)
   const [showPrevCourses, setShowPrevCourses] = useState(false)
+  const [currentCourses, setCurrentCourses] = useState(currCourses)
+  const [prevCourses, setPrevCourses] = useState(currCourses)
+
+  const navigate = useNavigate()
 
   const toggleCurrentCourses = () => {
     setShowCurrentCourses(!showCurrentCourses)
@@ -29,23 +49,40 @@ export default function MyCourses() {
     setShowPrevCourses(!showPrevCourses)
   }
 
+  const handleCourseClick = (course) => {
+    console.log('visiting course ' + course)
+    navigate(`/courses/${course}`)
+  }
+
+  const handleMyCourseClick = (course) => {
+    console.log('visiting my course ' + course)
+    navigate(`/my-courses/${course}`)
+  }
+
   return (
     <Stack
-      divider={<Divider orientation="horizontal" flexItem />}
+      // divider={<Divider orientation="horizontal" flexItem />}
       spacing={2}
       width={"100%"}
       textAlign="left"
       margin={"1rem 5rem"}
     >
+      <Typography
+        align="center"
+        variant="h2"
+        gutterBottom={true}
+      >
+        My Courses
+      </Typography>
       <Card variant="outlined">
         <CardContent>
           <Stack
             direction={"row"}
+            alignItems={"center"}
           >
             <Typography sx={{ fontSize: 20 }}>
               Current Courses
             </Typography>
-
             <ExpandMoreComponent
               expand={showCurrentCourses}
               onClick={toggleCurrentCourses}
@@ -55,13 +92,76 @@ export default function MyCourses() {
               <ExpandMore />
             </ExpandMoreComponent>
           </Stack>
+          
         </CardContent>
         <Collapse in={showCurrentCourses} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
+        <Stack
+            spacing={2}
+            width={"100%"}
+            textAlign="left"
+            justifyContent={"center"}
+            alignContent="center"
+          >
+            <Stack
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+          >
+            <Typography sx={{ fontSize: 16 }}>
+              Term: {currCourses.term}
+            </Typography>
+            <Button
+              variant='contained'
+              sx={{ fontSize: 16 }}
+            >
+              <Typography sx={{ fontSize: 16 }}>
+                Add Course            
+              </Typography>
+                
+            </Button>
+          </Stack>
+
+            {
+              currentCourses.courses.map((course) => {
+                return (
+                  <Card 
+                    variant="outlined"
+                  >
+                    <CardContent>
+                      <Stack
+                        direction={"row"}
+                        alignItems={"center"}
+                        justifyContent={"space-between"}
+                      >
+                        <Typography variant='h6'>
+                          {course.programCode} {course.courseCode} - {course.name}
+                        </Typography>
+                        <Stack
+                            direction={"row"}
+                            alignItems={"center"}
+                            spacing={1}
+                        >
+                          <Button
+                            variant="outlined"
+                            onClick={() => handleMyCourseClick(`${course.programCode}_${course.courseCode}`)}
+                          >
+                            Grades
+                          </Button>
+                          <Button
+                            variant="outlined"
+                            onClick={() => handleCourseClick(`${course.programCode}_${course.courseCode}`)}
+                          >
+                            Info
+                          </Button>
+                        </Stack>
+                      </Stack>
+                    </CardContent>
+                  </Card>
+                )
+              })
+            }
+          </Stack>
         </CardContent>
       </Collapse>
       </Card>
@@ -69,6 +169,7 @@ export default function MyCourses() {
         <CardContent>
           <Stack
             direction={"row"}
+            alignItems={"center"}
           >
             <Typography sx={{ fontSize: 20 }}>
               Previous Courses
@@ -86,10 +187,71 @@ export default function MyCourses() {
         </CardContent>
         <Collapse in={showPrevCourses} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
+        <Stack
+            spacing={2}
+            width={"100%"}
+            textAlign="left"
+            justifyContent={"center"}
+            alignContent="center"
+          >
+            <Stack
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+          >
+            <Typography sx={{ fontSize: 16 }}>
+              Term: {currCourses.term}
+            </Typography>
+            <Button
+              variant='contained'
+              sx={{ fontSize: 16 }}
+            >
+              <Typography sx={{ fontSize: 16 }}>
+                Add Course            
+              </Typography>
+                
+            </Button>
+          </Stack>
+            {
+              prevCourses.courses.map((course) => {
+                return (
+                  <Card 
+                    variant="outlined"
+                  >
+                    <CardContent>
+                      <Stack
+                        direction={"row"}
+                        alignItems={"center"}
+                        justifyContent={"space-between"}
+                      >
+                        <Typography variant='h6'>
+                          {course.programCode} {course.courseCode} - {course.name}
+                        </Typography>
+                        <Stack
+                            direction={"row"}
+                            alignItems={"center"}
+                            spacing={1}
+                        >
+                          <Button
+                              variant="outlined"
+                              onClick={() => handleMyCourseClick(`${course.programCode}_${course.courseCode}`)}
+                            >
+                              Grades
+                            </Button>
+                          <Button
+                            variant="outlined"
+                            onClick={() => handleCourseClick(`${course.programCode}_${course.courseCode}`)}
+                          >
+                            Go
+                          </Button>
+                        </Stack>
+                      </Stack>
+                    </CardContent>
+                  </Card>
+                )
+              })
+            }
+          </Stack>
         </CardContent>
       </Collapse>
       </Card>
