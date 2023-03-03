8e73331bdebc6143d84d6a5d5caae8a127098e87
YWDCHEN <ywdchen@uwaterloo.ca>
2023-03-03 11:08:21 -0500

    I drag the whole fronted folder into the workplace

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,24 +1,20 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 import styled from 'styled-components'
+import {Grid, Typography} from "@mui/material";
 import { Button } from '@mui/material'
-
+import {Link} from 'react-router-dom'
 const HomeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center
-`
+  height: 100vh;
+  width: 100vw;
+`;
 
-const HomeCol = styled.div`
-  display: grid;
-  grid-template-columns: 12rem 12rem 12rem 12rem;
-  grid-template-rows: repeat(4, 12rem);
-  width: 64rem;
-  justify-items: center;
-  align-items: center;
-  padding: 0 8rem;
-  box-sizing: border-box;
-`
+const Divider = styled.div`
+  width: 120px;
+  height: 10px;
+  background: #3c87f5;
+  margin-top: 40px;
+`;
+
 
 const StyledLink = styled(Link)`
   text-decoration: none;
@@ -34,50 +30,74 @@ const StyledButton = styled(Button)`
 export default function Home() {
   return (
     <HomeContainer>
-      <h2>I managed ur mom last night</h2>
-      <HomeCol>
-        <StyledButton variant="contained">
-          <StyledLink>
-              button
-          </StyledLink>
-        </StyledButton>      
-        <StyledButton variant="contained">
-          <StyledLink>
-              button
-          </StyledLink>
-        </StyledButton>
-        <StyledButton variant="contained">
-          <StyledLink>
-              button
-          </StyledLink>
-        </StyledButton>
+      <Grid className={'h-100'} container>
+        <Grid item xs={6} className={'h-100 bg-black p-5 text-white'}>
+          <h5 className={'text-start'}>FOREWORD</h5>
+          <h3 className={'mt-5 text-start'}>
+            WHAT'S MANAGEMENT ENGINEERING?
+          </h3>
+          <Divider></Divider>
+          <p className={'mt-5 text-start w-75 h6'}>
+            Management Engineering began as Waterloo'sversion ofan ndustrial Engineering program
+            Since then.courses have evolved to focus onbringing problem solving and decision making toany industryur courses include operationsresearch,optimization, statistics, software
+            development and design.
+          </p>
+        </Grid>
+        <Grid item xs={6} className={'h-100 p-5'}>
+          <Typography variant={'h4'} color={'#3c87f5'} className={'fw-bold text-start'}>
+            COMMON CAREER PATHS
+          </Typography>
+          <div className={'mt-4'}>
+            <Grid container spacing={3}>
+              <Grid item xs={4}>
+                <StyledButton variant="contained">
+                  <StyledLink>
+                    button
+                  </StyledLink>
+                </StyledButton>
+              </Grid>
+              <Grid item xs={4}>
+                <StyledButton variant="contained">
+                  <StyledLink>
+                    button
+                  </StyledLink>
+                </StyledButton>
+              </Grid>
+              <Grid item xs={4}>
+                <StyledButton variant="contained">
+                  <StyledLink>
+                    button
+                  </StyledLink>
+                </StyledButton>
+              </Grid>
+              <Grid item xs={4}>
+                <StyledButton variant="contained">
+                  <StyledLink>
+                    button
+                  </StyledLink>
+                </StyledButton>
+              </Grid>
+              <Grid item xs={4}>
+                <StyledButton variant="contained">
+                  <StyledLink>
+                    button
+                  </StyledLink>
+                </StyledButton>
+              </Grid>
+              <Grid item xs={4}>
+                <StyledButton variant="contained">
+                  <StyledLink>
+                    button
+                  </StyledLink>
+                </StyledButton>
+              </Grid>
 
-              <StyledButton variant="contained">
-          <StyledLink>
-              button
-          </StyledLink>
-        </StyledButton>
-        <StyledButton variant="contained">
-          <StyledLink>
-              button
-          </StyledLink>
-        </StyledButton>
-        <StyledButton variant="contained">
-          <StyledLink>
-              button
-          </StyledLink>
-        </StyledButton>
-        <StyledButton variant="contained">
-          <StyledLink>
-              button
-          </StyledLink>
-        </StyledButton>
-        <StyledButton variant="contained">
-          <StyledLink>
-              button
-          </StyledLink>
-        </StyledButton>
-      </HomeCol>
+            </Grid>
+
+
+          </div>
+        </Grid>
+      </Grid>
     </HomeContainer>
   )
 }
